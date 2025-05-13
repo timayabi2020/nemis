a5a256d638eafde8838930b0c1574ed03bb07d5c
Timothy Wamalwa <timwamalwa@gmail.com>
2025-05-13 09:29:18 +0300

    Intitial commit

---
+++ techhack/src/main/java/com/nemis/techhack/TechhackApplication.java
@@ -0,0 +1,13 @@
+package com.nemis.techhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechhackApplication.class, args);
+	}
+
+}
---
+++ techhack/src/test/java/com/nemis/techhack/TechhackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemis.techhack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
